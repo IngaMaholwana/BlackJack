9e3dc962870c442e189577482297c515f2db4438
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2024-12-16 22:23:14 +0200

    play button

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 let firstCard = 6
-let secondCard = 9
+let secondCard = 15
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+function startGame(){
+    if (sum <=  20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "Udoyile.You're out of the game! 😭"
+        isAlive = false
+    } 
+    messageEl.textContent = message
+}
  
-if (sum <=  20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "Udoyile.You're out of the game! 😭"
-    isAlive = false
-} 
-
-console.log(message)
