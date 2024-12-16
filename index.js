b8b76665f0fee7dc6d51cb45594baf76b2f98807
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2024-12-16 22:55:37 +0200

    the card element now displays on the dom from js

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+//another way to get element from the dom the query since its more dynamic have to be specific with ot 
+// let sumEl = document.querySelector("#sum-el") //# means id  class means dot
+let cardsEl = document.getElementById("cards-el")
+
+
 function startGame(){
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "sum: " + sum
     if (sum <=  20) {
         message = "Do you want to draw a new card? 🙂"
     } else if (sum === 21) {
